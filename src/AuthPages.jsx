1df925ac6c43e1533d176e2b0875a353366e2f37
natecruz <natecruzbtc2@gmail.com>
2026-06-14 11:07:13 +0300

    add tawk.to live chat on all pages

--- src/AuthPages.jsx
+++ src/AuthPages.jsx
@@ -158,13 +158,6 @@ export function LandingPage() {
       {/* Footer */}
       <Footer />
 
-      {/* Live Chat - Tawk.to */}
-      <script dangerouslySetInnerHTML={{ __html: `
-        var Tawk_API=Tawk_API||{},Tawk_LoadStart=new Date();
-        (function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;s1.src='https://embed.tawk.to/682cba5f50abe41913d56ca1/1irm5mf05';
-        s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();
-      `}} />
       </div>
     </div>
   );
