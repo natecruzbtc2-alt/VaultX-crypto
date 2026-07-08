1549aeeb800e86d71421bae57e3ca78b1b2ab9d9
natecruz <natecruzbtc2@gmail.com>
2026-07-08 19:35:44 +0300

    Fix hexagon - change fixed to absolute

--- src/CryptoBackground.jsx
+++ src/CryptoBackground.jsx
@@ -108,7 +108,7 @@ export default function CryptoBackground() {
 
   return (
     <canvas ref={canvasRef} style={{
-      position:"fixed", top:0, left:0, width:"100%", height:"100%",
+      position:"absolute", top:0, left:0, width:"100%", height:"100%",
       pointerEvents:"none", zIndex:0, display:"block",
     }}/>
   );
